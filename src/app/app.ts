43ce4d06b6989cde8cecc7db7a74ff64db24e11f
Juan Delgado <soy.juan.delgado@gmail.com>
2025-07-28 21:20:27 +0200

    get data from TMDB API

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { Component, inject, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
+import { TMDBApiService } from './shared/tmdb-api-service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'IT-S7-TMDB-API';
+  api = inject(TMDBApiService);
+  movies = signal({});
+
+  //TODO: cargar peliculas al inicio
+  //TODO: controlar el tiempo de espera
+  getMovies() {
+    this.movies.set(this.api.getMovies());
+    console.log(this.movies());
+  }
 }
