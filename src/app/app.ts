c009b6d8d1c49fab9494957231449921f032a078
Juan Delgado <soy.juan.delgado@gmail.com>
2025-07-28 13:35:57 +0200

    feat: initial project #1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
