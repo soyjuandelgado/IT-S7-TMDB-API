54c0034c9ceff3ad3195a220e8cf6679f202111d
Juan Delgado <soy.juan.delgado@gmail.com>
2025-07-29 11:11:22 +0200

    show movies list

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 import { TMDBApiService } from './shared/tmdb-api-service';
+import { IMovies } from './shared/imovies';
 
 @Component({
   selector: 'app-root',
@@ -11,15 +12,22 @@ import { TMDBApiService } from './shared/tmdb-api-service';
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   protected title = 'IT-S7-TMDB-API';
   api = inject(TMDBApiService);
-  movies = signal({});
+  movies = signal<IMovies>({
+    dates: { maximum: '', minimum: '' },
+    page: 0,
+    results: [],
+  });
 
   //TODO: cargar peliculas al inicio
-  //TODO: controlar el tiempo de espera
-  getMovies() {
-    this.movies.set(this.api.getMovies());
-    console.log(this.movies());
+  ngOnInit() {
+    this.api.getMovies().subscribe((response) => {
+      this.movies.set(response);
+      console.log(response);
+      console.log(this.movies());
+    });
   }
+  //TODO: controlar el tiempo de espera
 }
--- src/app/shared/tmdb-api-service.ts
+++ src/app/shared/tmdb-api-service.ts
@@ -1,60 +1,28 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-
-const TMDB_API_TOKEN =
-  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMjE1YWY4NDdjYzE0NzQwOWFjZjQxOTdlZGE0NDExNCIsIm5iZiI6MTc1MzY4NDUzNy4wNzksInN1YiI6IjY4ODcxYTM5ZDQzY2JjOGExMzU1ZjE1MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.K5IFiAMSxrZ3CRcrtm6yhKfTTKoW6GMclWSrq_PxkWc';
-const TMDB_NOW_PLAYING_URL = 'https://api.themoviedb.org/3/movie/now_playing';
-
-//TODO: Extraer interfaces
-export interface Movies {
-  dates: Dates
-  page: number
-  results: Movie[]
-}
-
-export interface Dates {
-  maximum: string
-  minimum: string
-}
-
-export interface Movie {
-  adult: boolean
-  backdrop_path: string
-  genre_ids: number[]
-  id: number
-  original_language: string
-  original_title: string
-  overview: string
-  popularity: number
-  poster_path: string
-  release_date: string
-  title: string
-  video: boolean
-  vote_average: number
-  vote_count: number
-}
+import { IMovies } from './imovies';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TMDBApiService {
+  private http = inject(HttpClient);
+  headers = new HttpHeaders().set('Authorization', `Bearer ${environment.TMDB_API_TOKEN}`);
+  params = new HttpParams().set('language', 'es-ES');
+  //.set("page", "1")
 
-  private http = inject(HttpClient)
-  //TODO: Usar environment
-  headers = new HttpHeaders().set('Authorization', `Bearer ${TMDB_API_TOKEN}`)
-  params = new HttpParams().set("language", "es-ES")
-    //.set("page", "1")
-
-  getMovies(){
-
+  getMovies() {
     //TODO: control de errores
-    return this.http.get<Movies>(TMDB_NOW_PLAYING_URL, {params: this.params, headers: this.headers}).subscribe( result => {
-      
-      console.log(result)
-      console.log(result["results"][0])
-      return result
-    })
+    return this.http
+      .get<IMovies>(environment.TMDB_NOW_PLAYING_URL, {
+        params: this.params,
+        headers: this.headers,
+      })
+      // .subscribe((result) => {
+      //   console.log(result);
+      //   console.log(result['results'][0]);
+      //   return result;
+      // });
   }
-
 }
