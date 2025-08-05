2170082377132bf76405d3e818bae4bc627ac2c1
Juan Delgado <soy.juan.delgado@gmail.com>
2025-08-05 19:45:28 +0200

    feat: firebase installed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,8 @@ import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,6 +18,6 @@ export const appConfig: ApplicationConfig = {
       routes,
       withInMemoryScrolling({ scrollPositionRestoration: 'top' })
     ),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch()), provideFirebaseApp(() => initializeApp({ projectId: "it-s7-tmdb-api-firebase", appId: "1:229472691931:web:5210a034db45f6402c0efe", storageBucket: "it-s7-tmdb-api-firebase.firebasestorage.app", apiKey: "AIzaSyDUC53Z2ZtT8zGuB-EjEvvA7ly8IOkmeRw", authDomain: "it-s7-tmdb-api-firebase.firebaseapp.com", messagingSenderId: "229472691931" })), provideAuth(() => getAuth()),
   ],
 };
