28ab9cbe8aa8debf0d8b611d55f5787844c094c9
Juan Delgado <soy.juan.delgado@gmail.com>
2025-08-05 19:55:12 +0200

    fix: project name in lowecase for firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -18,6 +18,17 @@ export const appConfig: ApplicationConfig = {
       routes,
       withInMemoryScrolling({ scrollPositionRestoration: 'top' })
     ),
-    provideHttpClient(withFetch()), provideFirebaseApp(() => initializeApp({ projectId: "it-s7-tmdb-api-firebase", appId: "1:229472691931:web:5210a034db45f6402c0efe", storageBucket: "it-s7-tmdb-api-firebase.firebasestorage.app", apiKey: "AIzaSyDUC53Z2ZtT8zGuB-EjEvvA7ly8IOkmeRw", authDomain: "it-s7-tmdb-api-firebase.firebaseapp.com", messagingSenderId: "229472691931" })), provideAuth(() => getAuth()),
+    provideHttpClient(withFetch()),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'it-s7-tmdb-api-firebase',
+        appId: '1:229472691931:web:5210a034db45f6402c0efe',
+        storageBucket: 'it-s7-tmdb-api-firebase.firebasestorage.app',
+        apiKey: 'AIzaSyDUC53Z2ZtT8zGuB-EjEvvA7ly8IOkmeRw',
+        authDomain: 'it-s7-tmdb-api-firebase.firebaseapp.com',
+        messagingSenderId: '229472691931',
+      })
+    ),
+    provideAuth(() => getAuth()),
   ],
 };
